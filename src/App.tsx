da063c2da3b02a42592a2920045c6edd65d8bed4
cllojan <cesarlojan8@gmail.com>
2024-01-25 19:57:05 -0500

    nextui tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 //import reactLogo from './assets/react.svg'
 //import viteLogo from '/vite.svg'
 import './App.css'
-
+import {Button} from "@nextui-org/react";
 import MangaItem from "./Components/MangaItem";
 function App() {
 
@@ -12,6 +12,7 @@ function App() {
 			<div>
 				<div>
 					<input />
+          <Button color="primary">aaaButton</Button>
 				</div>
 				<div className='mangas__container'>
 					<MangaItem />
